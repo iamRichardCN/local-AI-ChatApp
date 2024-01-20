736064c62d87dec2ccd5ee4593534e697dcb4736
Ritchy <iamrichardcn@gmail.com>
2024-01-20 21:27:10 +0100

    modeified app.py

--- app.py
+++ app.py
@@ -1,20 +1,34 @@
 import streamlit as st
 
 
+def clear_input_field():
+    st.session_state.user_question=st.session_state.user_input
+    st.session_state.user_input= ""
+
+def set_send_input():
+    st.session_state.send_input = True
+    clear_input_field()
+
 def main():
     st.title("LOCAL MULTIMODAL GPT")
     Chat_contianer=st.container()
 
-    user_input = st.text_input('type your message here', key="user_input")
+    if "send_input" not in st.session_state:
+        st.session_state.send_input=False
+        st.session_state.user_question= ""
+
+
+    user_input = st.text_input('type your message here', key="user_input", on_change=set_send_input)
 
     send_button=st.button('send', key='send_button')
 
-    if send_button:
-        llm_response= "this is a response from the LLM model"
+    if send_button or st.session_state.send_input:
+        if st.session_state.user_question != "":
+            llm_response= "this is a response from the LLM model"
 
-        with Chat_contianer:
-            st.chat_message('user').write(user_input)
-            st.chat_message('AI').write('here is an answer')
+            with Chat_contianer:
+                st.chat_message('user').write(st.session_state.user_question)
+                st.chat_message('AI').write('here is an answer')
 
 if __name__=="__main__":
     main()
